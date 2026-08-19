f4e5a0b9a0eb620c491ae22c743816d2d2080012
SamBruce07 <s.bruce30@ncf.edu>
2026-08-19 17:31:53 +0000

    Finish code tutorial

--- script-2.R
+++ script-2.R
@@ -1,3 +1,13 @@
 library(tidyverse)
 
-# had a decent piece of korean fried turkey at dining hall
+hist_p <- ggplot(diamonds, aes(x = carat)) +
+	geom_histogram(binwidth = 0.1, boundary = 0, colour = "white") +
+	labs(
+		title = "Diamond Carat and Number of Diamonds",
+		subtitle = "Most diamonds in the dataset weigh less than one carat.",
+		x = "Carat",
+		y = "Number of diamonds",
+		caption = "Source: diamonds dataset from the ggplot2 package."
+	)
+
+ggsave(plot = hist_p, file = "diamonds.png")
